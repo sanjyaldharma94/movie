cbb362731fce3657ba3777bce5993ab73c4e6f9f
Dharma Sanjyal <rajsan44@gmail.com>
2017-12-07 21:27:58 -0800

    some comments

--- js/main.js
+++ js/main.js
@@ -1,33 +1,43 @@
-//Arrow function
+//Arrow function to be used when the document loads
 $(document).ready(() => {
-//  alert(1);
+//  saving the value from the search box when enter key is pressed
 $('#searchForm').on('submit', (e) =>{
   let searchText = $('#searchText').val();
   getMovies(searchText);
   e.preventDefault();
 });
 });
-//calling api
+//calling the omdb api using apikey
 function getMovies(searchText){
+  //apikey pulls out the data from the movie site
   axios.get('http://www.omdbapi.com?apikey=199ec0b9&s='+searchText)
+  //
   .then((response) => {
+    //logging the response received from the server
   console.log(response);
+  //setting a variable for the the data we will be pulling out from the server's response
   let movies = response.data.Search;
+  //assigning a variable for displaying the data
   let output ='';
+  //running a each loop to loop through the movie list
   $.each(movies, (index, movie) =>{
+    //the backquotes is used to write multiple lines of codes in jQuery
     output += `
-    <div class="col-md-3">
+    <div class="col-md-4">
      <div class="well text-center">
      <img src ="${movie.Poster}">
      <h5>${movie.Title}</h5>
-     <a onclick="movieSelected(${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
+     <a onclick="movieSelected(${movie.imdbID})" class="btn btn-primary" href="#">Movie Details</a>
      </div>
-    ;`
+     </div>
+    `;
   });
+  //displaying out in the webpage
   $("#movies").html(output);
   })
+  //to catch the error and log them
   .catch((err) =>{
-//console.log(err);
+console.log(err);
 });
 }
 function movieSelected(id){
@@ -40,7 +50,7 @@ function getMovie(){
   axios.get('http://www.omdbapi.com?apikey=199ec0b9&i='+movieId)
   .then((response) => {
   console.log(response);
-  let movie = respunse.data;
+  let movie = response.data;
 
   let output = `
 <div class="row">
